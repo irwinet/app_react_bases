c9c1b1a9fc64245dcfb90d393fd62cefd2b3ea6a
Irwin Estrada <irwinestradatorres@gmail.com>
2026-09-22 17:07:40 -0500

    feat: Refactor CounterEffect.tsx

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { gsap } from "gsap";  
 
 const MAXIMUN_COUNT=10;
 
@@ -17,13 +18,17 @@ export const CounterEffect = () => {
         // }
 
         if(counter<10) return;
-        console.log('%cSe llego al valor maximo', 'color: red;background-color: black;')
+        console.log('%cSe llego al valor maximo', 'color: red;background-color: black;');
+        gsap.to('h2',{y: -10, duration: 0.2, ease: 'ease.out'}).then(() => {
+            gsap.to('h2',{y: 0, duration: 1, ease: 'bounce.out'})
+        });
     }, [counter]) //Dependencias
     
 
     return (
         <>
-            <h1>CounterEffect: {counter}</h1>
+            <h1>CounterEffect:</h1>
+            <h2>{counter}</h2>
             <button onClick={handleClick}>
                 +1
             </button>

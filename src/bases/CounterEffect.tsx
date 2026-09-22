624e1fa6b9ae326425ef40e2c405b7366e8aceac
Irwin Estrada <irwinestradatorres@gmail.com>
2026-09-22 17:21:06 -0500

    feat: Refactor CounterEffect.tsx

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,15 +1,19 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { gsap } from "gsap";  
 
 const MAXIMUN_COUNT=10;
 
 export const CounterEffect = () => {
     const [counter, setCounter] = useState(5);
+    const counterElement = useRef<HTMLHeadingElement>(null);
+
     const handleClick = () => {
         //if(counter < MAXIMUN_COUNT) setCounter(counter + 1);
         setCounter(prev => Math.min(prev+1, MAXIMUN_COUNT));
     }
 
+    //useEffect
+    //useLayoutEffect : Trabajar con referencias HTML
     useEffect(() => {
         // Cuerpo
         // console.log('useEffect');
@@ -19,16 +23,18 @@ export const CounterEffect = () => {
 
         if(counter<10) return;
         console.log('%cSe llego al valor maximo', 'color: red;background-color: black;');
-        gsap.to('h2',{y: -10, duration: 0.2, ease: 'ease.out'}).then(() => {
-            gsap.to('h2',{y: 0, duration: 1, ease: 'bounce.out'})
-        });
+        
+        const tl = gsap.timeline();
+        tl.to(counterElement.current,{y: -10, duration: 0.2, ease: 'ease.out'})
+        .to(counterElement.current,{y: 0, duration: 1, ease: 'bounce.out'});
+                
     }, [counter]) //Dependencias
     
 
     return (
         <>
             <h1>CounterEffect:</h1>
-            <h2>{counter}</h2>
+            <h2 ref={counterElement}>{counter}</h2>
             <button onClick={handleClick}>
                 +1
             </button>
